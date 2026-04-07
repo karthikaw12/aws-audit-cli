76992460d70d32ecb83bae88a46eedbe58b49d50
karthikaw12 <karthiks.k2006@yahoo.co.in>
2026-04-07 17:28:40 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+REGIONS = [
+    "ap-south-1",
+    "us-east-1",
+    "eu-west-1"
+]
+
+# Approx monthly cost (simplified estimation)
+COST_MAP = {
+    "t2.micro": 8,
+    "t3.micro": 9,
+    "db.t3.micro": 15,
+    "standard_s3": 0.023  # per GB
+}
